a7e939dd9612de07cc90ad8e566cb3d88ec8010c
Wesley Alves de Sousa <wesleyalvessousa98@gmail.com>
2024-02-05 12:36:00 -0300

    Put wrap on flex div

--- dist/nutryplus-front-end/server/main.server.mjs
+++ dist/nutryplus-front-end/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4PMUJV7T.mjs";import"./chunk-WIGB4I7I.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-TXOOMACD.mjs";import"./chunk-WIGB4I7I.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
