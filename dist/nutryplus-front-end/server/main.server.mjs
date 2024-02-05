f4c7b1ede8e6ac83e6a8596b087756d6e1ed6797
Wesley Alves de Sousa <wesleyalvessousa98@gmail.com>
2024-02-05 13:01:09 -0300

    Adding mg-bt on images and reduce footer padding

--- dist/nutryplus-front-end/server/main.server.mjs
+++ dist/nutryplus-front-end/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-UA57THUZ.mjs";import"./chunk-WIGB4I7I.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-RBMA4GZM.mjs";import"./chunk-WIGB4I7I.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
