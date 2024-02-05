132bc39e0914402357da3220e3215f7f6881fac6
Wesley Alves de Sousa <wesleyalvessousa98@gmail.com>
2024-02-05 12:20:37 -0300

    Test

--- dist/nutryplus-front-end/server/main.server.mjs
+++ dist/nutryplus-front-end/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-E6UYQIEY.mjs";import"./chunk-WIGB4I7I.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-EWLSG5P3.mjs";import"./chunk-WIGB4I7I.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
