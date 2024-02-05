19c71711c446011a9220d43640f0cd78151d627d
Wesley Alves de Sousa <wesleyalvessousa98@gmail.com>
2024-02-05 11:50:15 -0300

    Update dist with paths

--- dist/nutryplus-front-end/server/main.server.mjs
+++ dist/nutryplus-front-end/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZRORSQ6E.mjs";import"./chunk-WIGB4I7I.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-3EXNOOC4.mjs";import"./chunk-WIGB4I7I.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
