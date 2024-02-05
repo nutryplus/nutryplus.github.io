d85eadf63b39f1e30545d19e1f2ed8145f327f97
Wesley Alves de Sousa <wesleyalvessousa98@gmail.com>
2024-02-05 12:04:51 -0300

    Remove dots from image path

--- dist/nutryplus-front-end/server/main.server.mjs
+++ dist/nutryplus-front-end/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-3EXNOOC4.mjs";import"./chunk-WIGB4I7I.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-E6UYQIEY.mjs";import"./chunk-WIGB4I7I.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
